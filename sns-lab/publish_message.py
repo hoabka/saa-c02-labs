3a4bfa7c3c17cb5990c42c16f29e18c34c6d5d2a
lzli3s <anh@3sfarm.io>
2022-01-19 20:07:26 +0700

    add labs

--- sns-lab/publish_message.py
+++ sns-lab/publish_message.py
@@ -1,6 +1,6 @@
 import boto3 
 
 sns = boto3.client('sns')
-sns.publish(TopicArn=topic_arn, 
-            Message="Hello", 
+sns.publish(TopicArn="arn:aws:sns:us-east-1:992728521074:test-sns", 
+            Message="Hello from python", 
             Subject="Welcome to SAA course!")
